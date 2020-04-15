23fe1c71e7438d5e28654bd17385835cbbac949b
tomasz wyrwa <wyrwa.t@gmail.com>
2020-04-15 15:25:58 +0200

    add x button to hamburger

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.burger-wrapper');
 const home = document.querySelector('.home');
-const welcome = document.querySelector('.welcome');
+const iconBurger = document.querySelector('.hamburger');
+const iconX = document.querySelector('.close');
 
 function menuOn() {
     home.classList.toggle('menu-on');
-    welcome.classList.toggle('welcome-show');
+    iconBurger.classList.toggle('show');
+    iconX.classList.toggle('show');
 }
 
 hamburger.addEventListener('click', menuOn);
