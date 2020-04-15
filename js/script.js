7a2b1ec8fc9cfc42b352193458af46e26c52c018
tomasz wyrwa <wyrwa.t@gmail.com>
2020-04-15 14:52:18 +0200

    add media to jumbotron.scss

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const home = document.querySelector('.home');
+const welcome = document.querySelector('.welcome');
 
 function menuOn() {
     home.classList.toggle('menu-on');
+    welcome.classList.toggle('welcome-show');
 }
 
 hamburger.addEventListener('click', menuOn);
